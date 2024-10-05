5bef23597d5558a42ed42ddc59b3b6b9f6000151
JRinaldi <jrinaldi@jayrinaldi.me>
2024-10-06 03:02:41 +0900

    remove stream translate

--- ai_interface/ollama_services.py
+++ ai_interface/ollama_services.py
@@ -15,26 +15,14 @@ class OllamaTranslationService(TranslationService):
         Translate the text to the {LANGUAGE} language and only return the translated text.
         Do **not** state the original input and do **NOT** summarize!
         """
-        )
+        ).strip()
 
     async def translate(self, text: str, target_language: str) -> str:
         prompt = (
             self.SYSTEM_PROMPT_TRANSLATE.format(LANGUAGE=target_language)
             + "\n\n"
-            + text
+            + f"```text\n{text}\n```"
         )
 
         response = await self.client.generate(model=self.model, prompt=prompt)
         return response["response"].strip()
-
-    async def stream_translate(self, text: str, target_language: str):
-        prompt = (
-            self.SYSTEM_PROMPT_TRANSLATE.format(LANGUAGE=target_language)
-            + "\n\n"
-            + text
-        )
-
-        async for part in self.client.generate(
-            model=self.model, prompt=prompt, stream=True
-        ):
-            yield part["response"]
